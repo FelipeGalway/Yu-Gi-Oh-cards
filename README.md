09b1f649ae264cc4663d3d0b28c0e9a41e46ba0e
Felipe Ferreira Rezende <you@example.com>
2024-09-24 17:06:20 -0300

    Commit final

---
+++ README.md
@@ -0,0 +1,49 @@
+## Definição
+Este é um projeto React que consome uma API pública para fornecer informações detalhadas sobre cartas do jogo Yu-Gi-Oh!. Através deste projeto, você pode acessar dados como o nome da carta, tipo, raça, atributo, nível, ataque, defesa e muito mais.
+
+## Tecnologias Utilizadas
+- React
+- JavaScript
+- HTML
+- CSS
+
+## Como Usar
+Para usar a aplicação, siga estes passos simples:
+
+1. Clone este repositório em sua máquina local usando o seguinte comando:
+git clone https://seu-repositorio.git
+
+2. Navegue até o diretório do projeto:
+cd nome-do-diretorio
+
+3. Instale as dependências necessárias:
+npm install
+
+4. Inicie o servidor de desenvolvimento:
+npm start
+
+5. Abra o navegador e acesse http://localhost:3000 para visualizar a aplicação.
+
+6. Adicione o nome da carta desejada no campo de busca, conforme o tipo de cada carta. A API responderá com os dados da carta solicitada.
+
+## Exemplos de cartas que são retornadas pela API
+Cartas tipo Monstro (Monster):
+- Dark Magician
+- Blue-Eyes White Dragon
+- Ancient Lizard Warrior
+- Red-Eyes Black Dragon
+
+Cartas tipo Feitiço (Spell):
+- Nightmare's Steelcage
+- World Legacy Key
+- Vision with Eyes of Blue
+- Ultimate Fusion
+
+Cartas tipo Armadilha (Trap):
+- World Legacy Whispers
+- Icejade Erosion
+- Dinomorphia Domain
+- D/D/D Headhunt
+
+## Observação
+Certifique-se de que sua conexão com a Internet está ativa para que a aplicação possa buscar os dados da API corretamente. É importante também se atentar aos campos de busca e pesquisar a carta no campo correto.
---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import "./style.css";
+
+const API_URL = 'https://db.ygoprodeck.com/api/v7/cardinfo.php';
+
+function App() {
+  const [monsterCardName, setMonsterCardName] = useState('');
+  const [spellCardName, setSpellCardName] = useState('');
+  const [trapCardName, setTrapCardName] = useState('');
+  const [cardData, setCardData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchCardInfo = async (cardName, cardType) => {
+    try {
+      const response = await fetch(`${API_URL}?name=${cardName}`);
+      const data = await response.json();
+
+      if (data.data && data.data.length > 0) {
+        const filteredCards = data.data.filter(card => card.type.includes(cardType));
+        if (filteredCards.length > 0) {
+          setCardData({ data: filteredCards });
+          setError(null);
+        } else {
+          setError(`No ${cardType} cards found with that name!`);
+          setCardData(null);
+        }
+      } else {
+        setError("Card not found in the database! Try another section type or check the spelling!");
+        setCardData(null);
+      }
+    } catch (error) {
+      console.error('Error fetching card info:', error);
+      setError("Error fetching card info");
+      setCardData(null);
+    }
+  };
+
+  const handleMonsterSearch = (e) => {
+    e.preventDefault();
+    fetchCardInfo(monsterCardName, 'Monster');
+    setMonsterCardName('');
+  };
+
+  const handleSpellSearch = (e) => {
+    e.preventDefault();
+    fetchCardInfo(spellCardName, 'Spell Card');
+    setSpellCardName('');
+  };
+
+  const handleTrapSearch = (e) => {
+    e.preventDefault();
+    fetchCardInfo(trapCardName, 'Trap Card');
+    setTrapCardName('');
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h3>Felipe Ferreira &copy;</h3>
+      </header>
+
+      <div className="banner"></div>
+
+      <h1>Yu-Gi-Oh! Card Search</h1>
+
+      <div className="banner"></div>
+
+      <form onSubmit={(e) => { e.preventDefault(); }}>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Enter Monster Card name"
+            value={monsterCardName}
+            onChange={(e) => setMonsterCardName(e.target.value)}
+          />
+          <button type="submit" onClick={handleMonsterSearch}>Search Monster Card</button>
+        </div>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Enter Spell Card name"
+            value={spellCardName}
+            onChange={(e) => setSpellCardName(e.target.value)}
+          />
+          <button type="submit" onClick={handleSpellSearch}>Search Spell Card</button>
+        </div>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Enter Trap Card name"
+            value={trapCardName}
+            onChange={(e) => setTrapCardName(e.target.value)}
+          />
+          <button type="submit" onClick={handleTrapSearch}>Search Trap Card</button>
+        </div>
+      </form>
+
+      {error && <p>{error}</p>}
+      {cardData && !error && (
+        <div className="card-info">
+          <h2>{cardData.data[0].name}</h2>
+          <p>Level/Rank: {cardData.data[0].level}</p>
+          <p>Attack: {cardData.data[0].atk}</p>
+          <p>Defense: {cardData.data[0].def}</p>
+          <img src={cardData.data[0].card_images[0].image_url} alt={monsterCardName || spellCardName || trapCardName} />
+        </div>
+      )}
+
+      <div className="banner"></div>
+
+      <footer>
+        <h3>Felipe Ferreira &copy;</h3>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
+
+
